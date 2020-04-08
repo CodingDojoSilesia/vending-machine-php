edb9006c1f31099c7d7d645c54097263772bcc96
Martin89PL <marcingladkowski@gmail.com>
2020-04-08 21:29:45 +0200

    feat: Dime extends Money

--- src/Model/Dime.php
+++ src/Model/Dime.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace VendingMachine\Model;
 
-class Dime implements Coin
+class Dime extends Money implements Coin
 {
     /** @var int  */
     private int $value = 25;
 
-    public function value(): Value
+    public function value(): int
     {
         return $this->value;
     }
