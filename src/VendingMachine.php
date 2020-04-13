51a1b84d230968dd59604612fe30f3080d70d7db
Martin89PL <marcingladkowski@gmail.com>
2020-04-13 20:17:18 +0200

    feat: VendingMachine - execute BuyItemCommand service

--- src/VendingMachine.php
+++ src/VendingMachine.php
@@ -4,10 +4,49 @@ declare(strict_types=1);
 
 namespace VendingMachine;
 
-final class VendingMachine {
+use VendingMachine\Command\BuyItemCommand;
+use VendingMachine\Coordinator\PaymentCoordinator;
+use VendingMachine\Item\ItemB;
+use VendingMachine\Repository\ItemRepository;
+use VendingMachine\Request\BuyItemRequest;
+use VendingMachine\Request\ConsoleRequest;
+use VendingMachine\Response\ConsoleResponse;
+use VendingMachine\Util\InputParser;
+
+final class VendingMachine
+{
+
+    /**
+     * @var ItemRepository
+     */
+    private ItemRepository $itemRepository;
+    /**
+     * @var InputParser
+     */
+    private InputParser $inputParser;
+
+    public function __construct(ItemRepository $itemRepository, InputParser $inputParser)
+    {
+        $this->itemRepository = $itemRepository;
+        $this->inputParser = $inputParser;
+    }
 
     public function execute(string $input): string
     {
-        return $input;
+        $request = $this->parseInput($input);
+
+        $buyCommand = new BuyItemCommand($this->itemRepository, new PaymentCoordinator(), new ConsoleResponse());
+
+        $response = $buyCommand->execute(
+            new BuyItemRequest(new ItemB(), $request->moneyCollection())
+        );
+
+        return $response->getOutput();
+    }
+
+    private function parseInput(string $input): ConsoleRequest
+    {
+        /** @var ConsoleRequest $request */
+        return $this->inputParser->parse($input);
     }
 }
--- src/prompt.php
+++ src/prompt.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
 
+use VendingMachine\Item\ItemB;
+use VendingMachine\Repository\InMemoryItemRepository;
+use VendingMachine\Util\ConsoleInputParser;
 use VendingMachine\VendingMachine;
 
-$vm = new VendingMachine();
+
+$itemRepository = new InMemoryItemRepository();
+$itemRepository->add(new ItemB());
+
+
+$vm = new VendingMachine($itemRepository, new ConsoleInputParser());
 $input = '';
 
 while ($input !== 'exit') {
@@ -15,7 +23,9 @@ while ($input !== 'exit') {
 
     switch ($input) {
         case 'service':
-            echo $vm->execute() . "\n";
+            echo $vm->execute($input) . "\n";
             break;
+        default:
+            echo $vm->execute($input) . "\n";
     }
 }
