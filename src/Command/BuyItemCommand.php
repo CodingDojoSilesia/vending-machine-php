ac6f452c3e631ec3f4359ba9adf8bf68a61e6d33
Martin89PL <marcingladkowski@gmail.com>
2020-04-13 15:24:38 +0200

    ref: remove unused use statements

--- src/Command/BuyItemCommand.php
+++ src/Command/BuyItemCommand.php
@@ -4,17 +4,54 @@ declare(strict_types=1);
 
 namespace VendingMachine\Command;
 
-class BuyItemCommand implements Command
-{
-    private $input;
+use VendingMachine\Coordinator\PaymentCoordinator;
+use VendingMachine\Repository\ItemRepository;
+use VendingMachine\Request\BuyItemRequest;
+use VendingMachine\Response\ConsoleResponse;
 
-    public function __construct($input)
-    {
-        $this->input = $input;
+class BuyItemCommand
+{
+    /**
+     * @var ItemRepository
+     */
+    private ItemRepository $itemRepository;
+
+    /**
+     * @var PaymentCoordinator
+     */
+    private PaymentCoordinator $paymentCoordinator;
+
+    /**
+     * @var ConsoleResponse
+     */
+    private ConsoleResponse $response;
+
+    public function __construct(
+        ItemRepository $itemRepository,
+        PaymentCoordinator $paymentCoordinator,
+        ConsoleResponse $response
+    ) {
+        $this->itemRepository = $itemRepository;
+        $this->paymentCoordinator = $paymentCoordinator;
+        $this->response = $response;
     }
 
-    public function execute(): void
+    public function execute(BuyItemRequest $request): ConsoleResponse
     {
-        $this->input;
+        $item = $request->item();
+        $moneys = $request->moneyCollection();
+
+        $availableItem = $this->itemRepository->getItemBySelector($item->selector());
+
+        if ($availableItem === null) {
+            throw new \InvalidArgumentException(sprintf('Item %s is not available', $item->selector()));
+        }
+
+        if ($item->enougthToBuy($moneys->count())) {
+            $rest = $this->paymentCoordinator->pay($moneys->count(), $availableItem->value());
+            return $this->response->setRest($rest);
+        }
+
+        throw new \InvalidArgumentException('Set enough money to buy Item!');
     }
 }
--- src/Command/Command.php
+++ src/Command/Command.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace VendingMachine\Command;
+
+use VendingMachine\Request\Request;
 
 interface Command
 {
-    public function execute(): void;
+    public function execute(Request $request);
 }
